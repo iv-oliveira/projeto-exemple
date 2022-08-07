8617407414cf363223acc839238b99f6bbc26acb
ivsonoliveira <oliveira.ivson91@gmail.com>
2022-08-07 16:10:04 -0300

    Versão master

--- src/app.java
+++ src/app.java
@@ -1,6 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Olá GitHub");
-        System.out.println("Atualização");
+        System.out.println("Sistema versão master");
     }
 }
