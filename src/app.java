4a43e564fd2e07f02e5d66b09d666e4ca9088302
ivsonoliveira <oliveira.ivson91@gmail.com>
2022-08-07 16:36:08 -0300

    Nome e idade

--- src/app.java
+++ src/app.java
@@ -1,5 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Sistema versão moderna/criacao/nome-idade");
+        System.out.println("Nome: Ivson oliveira Idade: 30 anos");
     }
 }
